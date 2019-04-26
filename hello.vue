effc9e8a70baefa279a0afdd70ceb504b83ea9ba
任振鑫 <renzhenxin@sunmi.com>
2019-04-26 16:04:20 +0800

    vue学习demo

---
+++ hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+export default { //导出组件，在其他文件可以通过import引入组件
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+}
+</style>
